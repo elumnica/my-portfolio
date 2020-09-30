0af2824e2f2a4cf3e6c2a266cd022c957ffa2fa0
Elvis Lumnica <58716184+elumnica@users.noreply.github.com>
2020-09-29 22:48:36 -0400

    styled the about page

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Lottie from 'react-lottie';
 import Navbar from './navbar.jsx';
+import Elvis from '../img/elvis.JPG';
 import "../styles/about.css";
 
 function About() {
@@ -8,6 +9,9 @@ function About() {
     return (
     <div className='parent'>
         <Navbar />
+        <div className='elvis'>
+            <img src={Elvis}alt='elvis' width={600} height={500} />
+        </div>
     </div>   
     )
 }
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Site from "./site.jsx";
+import About from "./about.jsx";
 
 
 
 function App() {
     return(
         <div>
-            <Site />
+            <About />
         </div>
     );
 }
