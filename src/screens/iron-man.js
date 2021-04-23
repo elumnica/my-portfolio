7d94b460c74e203cc08993d42e514617558c7aa1
Elvis Lumnica <visi@Elviss-MBP.fios-router.home>
2021-04-22 21:44:55 -0400

    iron man project page sorta done

--- src/screens/iron-man.js
+++ src/screens/iron-man.js
@@ -1,20 +1,80 @@
 import React from 'react';
+import Lottie from 'react-lottie';
+import animationData from '../lotties/software-dev.json';
 import Navbar from '../components/navbar.jsx';
+import ProjectCard from '../components/projectCard.jsx';
 import Footer from '../components/footer.jsx';
-import './iron.css';
+import "./iron.css";
 
 function Iron() {
-
     return (
         <div>
                 <Navbar />
                 <section className="main-container">
                     <div>
-                        <h1>Iron Man Solidworks Model</h1>
+                        <h1 className='title'>Iron Man Helmet</h1>
+                        <span className='text-two'>CAD Designer • SOLIDWORKS • 3D Printing</span>
                     </div>
-                <div className='iron-assembly'>
-                </div>
                 </section>
+                <div className='assembly'>
+                        <img src={require('../img/iron-man-drawing.JPG')}></img>
+                </div>
+                <div className='problem'>
+                    <h2 className='title'>The Problem</h2>
+                    <p className='statement'>The task at hand was learning how to create a full assembly on SOLIDWORKS and then being able to  <br></br>3D print the design to create a prototype. 
+                    Much of the learning process was making sure that each  <br></br> of the parts were able to be assembled correctly. I learned a lot of different commands SOLIDWORKS  <br></br> offers which helped deepen my knowledge on the designing aspect of the software.</p>
+               
+                    <img className='three-model' src={require('../img/3D-iron-man.JPG')}></img>
+                </div>
+                <span className='work-caption'>☝️ 3D Model Assembly of an Iron Man Helmet Design ☝️ </span>
+
+               
+                <div className='problem'>
+                    <h2 className='title'>Parts</h2>
+                        <p className='statement'>A lot of the parts.</p>
+                </div>
+                <div className='row'>
+                        <div className='column'>
+                            <img className='four-model' src={require('../img/iron-mask.JPG')}></img>
+                            <span className='work-caption'>☝️ 3D Model of the mask shell ☝️ </span>
+                        </div>
+                        <div className='column'>
+                            <img className='four-model' src={require('../img/iron-top.JPG')}></img>
+                            <span className='work-caption'>☝️ 3D Model of the top shell ☝️ </span>
+                        </div>
+                </div>
+                <div className='row'>
+                        <div className='column'>
+                            <img className='four-model' src={require('../img/back-piece-ironman.JPG')}></img>
+                            <span className='work-caption'>☝️ 3D Model of the back shell ☝️ </span>
+                        </div>
+                        <div className='column'>
+                            <img className='four-model' src={require('../img/combustion-drawing.JPG')}></img>
+                            <span className='work-caption'>☝️ Annotation Drawing of the Combustion Chamber ☝️ </span>
+                        </div>
+                </div>
+                <div className='problem'>
+                    <h2 className='title'>Improvements/Analysis</h2>
+                    <p className='statement'>The next step I would take in progressing this project is to perform a stress/shock absorption analysis simulation on a <br></br> certain type of
+                    material I would choose. If I were to progress this design into a helmet for outside activity I would think <br></br>about different materials and layers 
+                    needed to provide impact protection. Often layers are used together to provide <br></br>impact protection. The outer layer is generally a hard shell or a micro-shell 
+                    made of fiberglass, Lexan or ABS plastic. <br></br>This shell serves many purposes: it distributes the force of the collision over a large area, it allows the helmet to slide, <br></br>
+                    thereby causing a slower deceleration; it provides a shield against puncture and it holds the middle layer together. <br></br>The middle layer is usually a crushable liner that absorbs the shock of collision. 
+                    This layer is often made of expanded <br></br>polystyrene, also known as EPS. The inner layer, 
+                    which may be more segmented, helps to ensure proper fit and comfort.<br></br></p>
+               </div>
+               //3D Printing
+               <div className='row'>
+                        <div className='column'>
+                            <img className='five-model' src={require('../img/pic1.JPG')}></img>
+                            <span className='work-caption'>☝️ 3D Print of the Helmet Design ☝️ </span>
+                        </div>
+                        <div className='column'>
+                            <img className='five-model' src={require('../img/pic2.JPG')}></img>
+                            <span className='work-caption'>☝️ Material used is ABS Plastic. ☝️ </span>
+                        </div>
+                </div>
+
                 <div className='footer'>
                     <Footer />
                 </div>
@@ -23,3 +83,5 @@ function Iron() {
 }
 
 export default Iron;
+
+
--- src/screens/jet-engine.js
+++ src/screens/jet-engine.js
@@ -43,16 +43,17 @@ function Engine() {
                         <p className='statement'>The compressor rotates to continuously pressurized gases. <br></br>Its primary function is to supply air in enough quantity to meet 
                         <br></br>the requirements of the combustion burners.</p>
                 </div>
-                <div className='row'>   
+                <div className='row'>
                         <div className='column'>
                             <img className='four-model' src={require('../img/propeller-model.JPG')}></img>
-                            <span className='work-caption'>☝️ 3D Model of the propeller ☝️ </span>
+                            <span className='work-caption'>☝️ 3D Model of the compressor ☝️ </span>
                         </div>
                         <div className='column'>
                             <img className='four-model' src={require('../img/propeller-drawings.JPG')}></img>
-                            <span className='work-caption'>☝️ Annotation Drawing of the propeller ☝️ </span>
+                            <span className='work-caption'>☝️ Annotation Drawing of the compressor ☝️ </span>
                         </div>
                 </div>
+                //Combustion Chamber
                 <div className='problem'>
                     <h2 className='title'>Parts: Combustion Chamber</h2>
                         <p className='statement'>Once air enters the front intake and is compressed, the air is forced into combustion chambers where <br></br>fuel is sprayed into it, and the mixture of air and fuel is ignited. Gases that form expand <br></br>rapidly and are exhausted through the rear of the combustion chambers.</p>
@@ -67,8 +68,72 @@ function Engine() {
                             <span className='work-caption'>☝️ Annotation Drawing of the Combustion Chamber ☝️ </span>
                         </div>
                 </div>
-                
-
+                //Mid Frame
+                <div className='problem'>
+                    <h2 className='title'>Parts: Mid Frame</h2>
+                        <p className='statement'>A gas turbine engine mid turbine frame having an inner case See application file for complete search history. Supporting at least one bearing and at least three spokes extending radially outwardly to an outer case, the mid turbine frame having an interturbine duct extending through the mid turbine frame, the interturbine duct spaced axially closer to an upstream turbine disc than a bearing. Supporting structure of the mid turbine frame and mounted axially slidingly relative to the bearing supporting structure to substantially isolate the bearing supporting structure from axial loads, for example such as disc loads incurred in the unlikely event a turbine disc shaft shears within the engine.</p>
+                </div>
+                <div className='row'>
+                        <div className='column'>
+                            <img className='four-model' src={require('../img/mid-frame-model.JPG')}></img>
+                            <span className='work-caption'>☝️ 3D Model of the Mid Frame System ☝️ </span>
+                        </div>
+                        <div className='column'>
+                            <img className='four-model' src={require('../img/mid-frame-drawing.JPG')}></img>
+                            <span className='work-caption'>☝️ Annotation Drawing of the Mid Frame System ☝️ </span>
+                        </div>
+                </div>
+                //Turbine
+                <div className='problem'>
+                    <h2 className='title'>Parts: Turbine</h2>
+                        <p className='statement'>The turbines are linked by a shaft to turn the blades in the compressor and to spin the intake fan at the front. This rotation takes some energy from the high-energy flow that is used to drive the fan and the compressor. The gases produced in the combustion chamber move through the turbine and spin its blades.</p>
+                </div>
+                <div className='row'>
+                        <div className='column'>
+                            <img className='four-model' src={require('../img/turbine-model.JPG')}></img>
+                            <span className='work-caption'>☝️ 3D Model of the Turbine ☝️ </span>
+                        </div>
+                        <div className='column'>
+                            <img className='four-model' src={require('../img/turbine-drawing.JPG')}></img>
+                            <span className='work-caption'>☝️ Annotation Drawing of the Turbine ☝️ </span>
+                        </div>
+                </div>
+                //Rear Frame System
+                <div className='problem'>
+                    <h2 className='title'>Parts: Rear Frame System</h2>
+                        <p className='statement'>A turbine rear frame for a gas turbine engine comprises a plurality of struts disposed between an outer ring and an inner ring. The struts can be mounted adjacent to one or more mount surfaces defined within the outer ring. The mount surface can comprise a recess in the outer ring having a maximum radial distance upstream of engine mounts inserted therein. The struts can further comprise a pitch angle offset from a centerline  of the mount surface and a tangentially curved trailing edge at a tip to improve aerodynamic performance of the turbine rear frame.</p>
+                </div>
+                <div className='row'>
+                        <div className='column'>
+                            <img className='four-model' src={require('../img/rear-frame-model.JPG')}></img>
+                            <span className='work-caption'>☝️ 3D Model of the Rear Frame ☝️ </span>
+                        </div>
+                        <div className='column'>
+                            <img className='four-model' src={require('../img/rear-frame-drawing.JPG')}></img>
+                            <span className='work-caption'>☝️ Annotation Drawing of the Rear Frame ☝️ </span>
+                        </div>
+                </div>
+                //Exhaust Nozzle
+                <div className='problem'>
+                    <h2 className='title'>Parts: Exhaust Nozzle</h2>
+                        <p className='statement'>The nozzle is the end module of the engine which accelerates the flow of exhaust gases out of the engine. As hot air from the engine core mixes with cooler air blowing through the engine fan, the shaped edges serve to smooth the mixing, which reduces turbulence that creates noise.</p>
+                </div>
+                <div className='row'>
+                        <div className='column'>
+                            <img className='four-model' src={require('../img/exhaust.JPG')}></img>
+                            <span className='work-caption'>☝️ 3D Model of the Exhaust Nozzle ☝️ </span>
+                        </div>
+                        <div className='column'>
+                            <img className='four-model' src={require('../img/exhuast-drawing.JPG')}></img>
+                            <span className='work-caption'>☝️ Annotation Drawing of the Exhaust Nozzle ☝️ </span>
+                        </div>
+                </div>
+                <div className='problem'>
+                    <h2 className='title'>Improvement/Analysis</h2>
+                    <p className='statement'>The next step I would take in progressing this project is to perform a flow and thermal simulation on the air flow 
+                    <br></br>and combustion processes that the jet engine take to verify design functionality. After confirming that 
+                    <br></br>both processes confirm design functionality, the next step would be to build a prototype for testing.</p>
+               </div>
                 <div className='footer'>
                     <Footer />
                 </div>
@@ -77,3 +142,5 @@ function Engine() {
 }
 
 export default Engine;
+
+
